2108b4fb17dfc6ffed325415859f30cd61e8a9e1
MissLadyPandas <kbrumback@ivytech.edu>
2023-02-07 18:34:23 -0500

    first commit

---
+++ people.js
@@ -0,0 +1,11 @@
+/* Reminders - Shift+Alt+A = Comment out Line */
+/* Reminders Shift+Alt+DownArrow = Copy Line Down  */
+
+const people = ['yoshi', 'ryu', 'chun-li', 'mario'];
+const ages = [20,25,30,35];
+
+console.log(people);
+
+module.exports = {
+    people, ages
+};
