03911e0e548de705928f4b0b27c3201bcd884af1
MissLadyPandas <kbrumback@ivytech.edu>
2023-02-07 18:48:13 -0500

    new file:   docs/blog3.txt 	new file:   docs/blog4.txt 	modified:   streams.js

---
+++ streams.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf8'});
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
+
+readStream.on('data', (chunk) => {
+    console.log('----- NEW CHUNK -----');
+    console.log(chunk.toString());
+    writeStream.write(chunk);
+    writeStream.write('\nNEWCHUNK\n');
+    writeStream.write(chunk);
+});
